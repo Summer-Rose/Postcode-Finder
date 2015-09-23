3d2b3d7996b3d651095454d78c31803108636976
Summer <sbrochtrup@gmail.com>
2015-09-23 08:21:01 -0700

    Add ajax API call and processXML function

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $("#findPostcode").click(function(event) {
+      event.preventDefault();
+      var address = encodeURIComponent($("#address").val());
+      var result=0;
+      $("#address").val("");
+      $("#alert").hide();
+    $.ajax({
+      type: "GET",
+      url: "https://maps.googleapis.com/maps/api/geocode/xml?address="+address+"&key=AIzaSyALxgnDY-pRt-5Gx2cF_NZ--jQWAMQBCm4",
+      dataType: "xml",
+      success: processXML
+    });
+
+    function processXML(xml) {
+      $(xml).find("address_component").each(function() {
+        if ($(this).find("type").text() == "postal_code") {
+          $("#success").fadeIn;
+          $("#success").html("<h5>Address Found</h5><p>The postcode for that address is "+$(this).find("long_name").text()+"</p>").fadeIn();
+          result=1;
+        }
+      });
+
+      if (result == 0) {
+        $("#fail").fadeIn();
+      }
+    };
+  });
+});
+
