2069accd733d476badfb894276084f8999dc508f
Summer <sbrochtrup@gmail.com>
2015-09-23 20:11:14 -0700

    Add function to display error div when api call fails

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,19 @@ $(document).ready(function() {
       type: "GET",
       url: "https://maps.googleapis.com/maps/api/geocode/xml?address="+address+"&key=AIzaSyALxgnDY-pRt-5Gx2cF_NZ--jQWAMQBCm4",
       dataType: "xml",
-      success: processXML
+      success: processXML,
+      error: error
     });
+
+    function error() {
+      $(".alert").hide();
+      $("#error").fadeIn();
+    }
 
     function processXML(xml) {
       $(xml).find("address_component").each(function() {
         if ($(this).find("type").text() == "postal_code") {
+          $(".alert").hide();
           $("#success").fadeIn;
           $("#success").html("<h5>Address Found</h5><p>The postcode for that address is "+$(this).find("long_name").text()+"</p>").fadeIn();
           result=1;
